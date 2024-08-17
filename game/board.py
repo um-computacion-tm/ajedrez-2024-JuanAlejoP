a7408ba2c15bc0ab4b49cbec1d7b558ef03922d4
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-16 22:01:20 -0300

    fix pieces positions

--- game/board.py
+++ game/board.py
@@ -1,4 +1,4 @@
-from game.pieces import Pawn, Rook, Knight, Bishop, Queen, King
+from game.pieces import Pawn, Rook, Bishop, Knight, Queen, King
 
 class Board:
     def __init__(self):
@@ -12,24 +12,24 @@ class Board:
         self.__positions__[0][0] = Rook('WHITE')
         self.__positions__[0][1] = Knight('WHITE')
         self.__positions__[0][2] = Bishop('WHITE')
-        self.__positions__[0][3] = Queen('WHITE')
-        self.__positions__[0][4] = King('WHITE')
+        self.__positions__[0][3] = King('WHITE')
+        self.__positions__[0][4] = Queen('WHITE')
         self.__positions__[0][5] = Bishop('WHITE')
         self.__positions__[0][6] = Knight('WHITE')
         self.__positions__[0][7] = Rook('WHITE')
 
-        self.__positions__[7][0] = Rook('BLACK')
-        self.__positions__[7][1] = Knight('BLACK')
-        self.__positions__[7][2] = Bishop('BLACK')
-        self.__positions__[7][3] = Queen('BLACK')
-        self.__positions__[7][4] = King('BLACK')
-        self.__positions__[7][5] = Bishop('BLACK')
-        self.__positions__[7][6] = Knight('BLACK')
-        self.__positions__[7][7] = Rook('BLACK')
-    
         for col in range(8):
             self.__positions__[1][col] = Pawn('WHITE')
             self.__positions__[6][col] = Pawn('BLACK')
+
+        self.__positions__[7][0] = Rook('BLACK')
+        self.__positions__[7][1] = Knight('BLACK')
+        self.__positions__[7][2] = Bishop('BLACK')
+        self.__positions__[7][3] = King('BLACK')
+        self.__positions__[7][4] = Queen('BLACK')
+        self.__positions__[7][5] = Bishop('BLACK')
+        self.__positions__[7][6] = Knight('BLACK')
+        self.__positions__[7][7] = Rook('BLACK')
 
     def get_piece(self, row, col):
         return self.__positions__[row][col]
