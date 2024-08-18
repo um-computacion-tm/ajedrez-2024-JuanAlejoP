93bec6420080a82043174266ebd3db52e0734b72
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-18 20:41:54 -0300

    add rook method

--- game/pieces.py
+++ game/pieces.py
@@ -10,6 +10,9 @@ class Rook(Piece):
     def __init__(self, colour):
         super().__init__(colour)
 
+    def rook_valid_move(self, from_row, from_col, to_row, to_col):
+        return from_row == to_row or from_col == to_col
+
 class Bishop(Piece):
     def __init__(self, colour):
         super().__init__(colour)
