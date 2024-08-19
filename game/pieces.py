5b4efb0a8489ae99229db8316bcb4fa08b466c13
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-19 20:48:43 -0300

    add valid_move for Bishop

--- game/pieces.py
+++ game/pieces.py
@@ -28,6 +28,10 @@ class Rook(Piece):
 class Bishop(Piece):
     def __init__(self, colour):
         super().__init__(colour)
+    
+    def valid_move(self, from_row, from_col, to_row, to_col):
+        if (to_row - from_row) != (to_col - from_col):
+                raise ValueError('La pieza Alfil solo puede moverse en diagonal.')
 
 class Knight(Piece):
     def __init__(self, colour):
