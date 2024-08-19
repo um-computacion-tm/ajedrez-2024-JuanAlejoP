666b57e9b1f4d605506aff8ddc24d2b3ceb81dda
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-19 20:56:34 -0300

    add valid_move for Queen

--- game/pieces.py
+++ game/pieces.py
@@ -40,6 +40,10 @@ class Knight(Piece):
 class Queen(Piece):
     def __init__(self, colour):
         super().__init__(colour)
+    
+    def valid_move(self, from_row, from_col, to_row, to_col):
+        if to_row or to_col not in range(0,7):
+            raise ValueError('La pieza no debe salir del tablero.')
 
 class King(Piece):
     def __init__(self, colour):
