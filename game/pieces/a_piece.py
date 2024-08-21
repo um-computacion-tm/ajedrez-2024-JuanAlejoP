72735e94855f37fe2120a7dd37c2390ab4117a1a
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-21 17:40:28 -0300

    transform class Piece into an abstract Class

--- game/pieces/a_piece.py
+++ game/pieces/a_piece.py
@@ -1,6 +1,13 @@
-class Piece:
+from abc import ABC, abstractmethod
+
+class Piece(ABC):
     def __init__(self, colour):
         self.__colour__ = colour
 
+    @abstractmethod
     def valid_move(self, from_row, from_col, to_row, to_col):
-        return True
+        pass
+
+    @property
+    def colour(self):
+        return self.__colour__
