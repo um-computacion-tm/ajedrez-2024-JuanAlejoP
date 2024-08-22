365deced9d395a5a4b412572ed9011cdedd86dac
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-22 00:15:47 -0300

    update client

--- game/client.py
+++ game/client.py
@@ -1,18 +1,5 @@
 from game.chess import Chess
-
-class ConsoleIO:
-    def input_move(self):
-        from_row = int(input('Fila Inicial: '))
-        from_col = int(input('Columna Inicial: '))
-        to_row = int(input('Fila Final: '))
-        to_col = int(input('Columna Final: '))
-        return from_row, from_col, to_row, to_col
-
-    def output_turn(self, turn):
-        print(f'Turno: {turn}')
-
-    def output_error(self, error_message):
-        print('ERROR!', error_message)
+from game.console_io import ConsoleIO
 
 class Game:
     def __init__(self, chess: Chess, io_handler: ConsoleIO):
@@ -21,13 +8,12 @@ class Game:
 
     def play(self):
         try:
+            self.io_handler.output_board(self.chess.board)
             self.io_handler.output_turn(self.chess.turn)
             from_row, from_col, to_row, to_col = self.io_handler.input_move()
             self.chess.move(from_row, from_col, to_row, to_col)
         except Exception as e:
             self.io_handler.output_error(str(e))
-
-
 
 def main():
     chess = Chess()
