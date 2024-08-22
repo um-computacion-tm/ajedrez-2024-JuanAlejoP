6a5e693cfa3fefde322bb44f19f644987df79af1
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-22 20:27:12 -0300

    update tests for Board

--- tests/tests_board.py
+++ tests/tests_board.py
@@ -1,42 +1,43 @@
 import unittest
-from game.board import Board, Pawn, Rook, Bishop, Knight, Queen, King
+from game.board import Board, Pawn, Rook, Bishop, Knight, Queen, King, BoardInitializer
 
 class TestBoard(unittest.TestCase):
 
+    def setUp(self):
+        self.board = Board()
+        initializer = BoardInitializer()
+        initializer.initialize(self.board)
+
     def test_initial_setup(self):
-        board = Board()
-        
-        self.assertIsInstance(board.get_piece(0, 0), Rook)
-        self.assertIsInstance(board.get_piece(0, 1), Knight)
-        self.assertIsInstance(board.get_piece(0, 2), Bishop)
-        self.assertIsInstance(board.get_piece(0, 3), King)
-        self.assertIsInstance(board.get_piece(0, 4), Queen)
-        self.assertIsInstance(board.get_piece(0, 5), Bishop)
-        self.assertIsInstance(board.get_piece(0, 6), Knight)
-        self.assertIsInstance(board.get_piece(0, 7), Rook)
+        self.assertIsInstance(self.board.get_piece(0, 0), Rook)
+        self.assertIsInstance(self.board.get_piece(0, 1), Knight)
+        self.assertIsInstance(self.board.get_piece(0, 2), Bishop)
+        self.assertIsInstance(self.board.get_piece(0, 3), King)
+        self.assertIsInstance(self.board.get_piece(0, 4), Queen)
+        self.assertIsInstance(self.board.get_piece(0, 5), Bishop)
+        self.assertIsInstance(self.board.get_piece(0, 6), Knight)
+        self.assertIsInstance(self.board.get_piece(0, 7), Rook)
 
-        for col in range(8):
-            self.assertIsInstance(board.get_piece(1, col), Pawn)
-            self.assertIsInstance(board.get_piece(6, col), Pawn)
-
-        self.assertIsInstance(board.get_piece(7, 0), Rook)
-        self.assertIsInstance(board.get_piece(7, 1), Knight)
-        self.assertIsInstance(board.get_piece(7, 2), Bishop)
-        self.assertIsInstance(board.get_piece(7, 3), King)
-        self.assertIsInstance(board.get_piece(7, 4), Queen)
-        self.assertIsInstance(board.get_piece(7, 5), Bishop)
-        self.assertIsInstance(board.get_piece(7, 6), Knight)
-        self.assertIsInstance(board.get_piece(7, 7), Rook)
-
-        for row in range(2, 6):
+        for row in range(8):
             for col in range(8):
-                self.assertIsNone(board.get_piece(row, col))
+                if row == 1 or row == 6:
+                    self.assertIsInstance(self.board.get_piece(row, col), Pawn)
+                elif 2 <= row <= 5:
+                    self.assertIsNone(self.board.get_piece(row, col))
+
+        self.assertIsInstance(self.board.get_piece(7, 0), Rook)
+        self.assertIsInstance(self.board.get_piece(7, 1), Knight)
+        self.assertIsInstance(self.board.get_piece(7, 2), Bishop)
+        self.assertIsInstance(self.board.get_piece(7, 3), King)
+        self.assertIsInstance(self.board.get_piece(7, 4), Queen)
+        self.assertIsInstance(self.board.get_piece(7, 5), Bishop)
+        self.assertIsInstance(self.board.get_piece(7, 6), Knight)
+        self.assertIsInstance(self.board.get_piece(7, 7), Rook)
    
     def test_get_piece(self):
-        board = Board()
-        piece = board.get_piece(0, 0)
-        self.assertEqual(piece.__class__.__name__, 'Rook')
-        self.assertEqual(piece.__colour__, 'WHITE')
+        piece = self.board.get_piece(0, 0)
+        self.assertIsInstance(piece, Rook)
+        self.assertEqual(piece.colour, 'WHITE')
 
     def test_within_bounds(self):
         ...
