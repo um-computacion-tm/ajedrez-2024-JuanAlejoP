3e6c2a413da69980629c5cdaa3aebc0578b9b623
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-22 20:43:36 -0300

    update tests for Chess

--- tests/tests_chess.py
+++ tests/tests_chess.py
@@ -1,6 +1,6 @@
 import unittest
 from game.chess import Chess
-from game.pieces import Pawn
+from game.pieces import *
 
 class TestChess(unittest.TestCase):
 
@@ -8,20 +8,21 @@ class TestChess(unittest.TestCase):
         self.chess = Chess()
 
     def test_move_piece(self):
-        self.chess.move(1, 0, 2, 0)
-        piece = self.chess.board.get_piece(2, 0)
-        self.assertIsInstance(piece, Pawn)
-        self.assertIsNone(self.chess.board.get_piece(1, 0))
+        self.chess.move(0, 0, 0, 3)
+        piece = self.chess.board.get_piece(0, 3)
+        self.assertIsInstance(piece, Rook)
+        self.assertIsNone(self.chess.board.get_piece(0, 0))
     
     def test_no_piece_error(self):
         with self.assertRaises(ValueError):
             self.chess.move(3, 3, 4, 4)
 
     def test_change_turn(self):
-        self.chess.move(1, 0, 2, 0)
+        self.chess.move(0, 0, 0, 3)
         self.assertEqual(self.chess.turn, 'BLACK')
-        self.chess.move(6, 0, 5, 0)
+        self.chess.move(7, 0, 7, 3)
         self.assertEqual(self.chess.turn, 'WHITE')
+
 
 
 if __name__ == '__main__':
