45085d6a91c95d67555de1bd97292ebe179e9262
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-23 17:08:44 -0300

    fix Pawn valid_move

--- game/pieces/pawn.py
+++ game/pieces/pawn.py
@@ -10,13 +10,10 @@ class Pawn(Piece):
     def valid_move(self, from_row, from_col, to_row, to_col):
         direction = 1 if self.colour == 'WHITE' else -1
         if from_col == to_col:
-            #Movimiento hacia adelante
             if to_row == from_row + direction:
                 return True
-            #Movimiento de dos casillas desde la posición inicial
             if (from_row == 1 or from_row == 6) and to_row == from_row + 2 * direction:
                 return True
         elif abs(from_col - to_col) == 1 and to_row == from_row + direction:
-            #Captura en diagonal
             return True
         return False
