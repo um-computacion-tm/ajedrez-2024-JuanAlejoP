da91d1050903a41ab9b75fb13598cf61dacfe012
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-23 10:52:46 -0300

    transform piece attribute colour into property

--- game/pieces/a_piece.py
+++ game/pieces/a_piece.py
@@ -4,6 +4,10 @@ class Piece(ABC):
     def __init__(self, colour):
         self.__colour__ = colour
 
+    @property
+    def colour(self):
+        return self.__colour__        
+
     @abstractmethod
     def valid_move(self, from_row, from_col, to_row, to_col):
         pass
