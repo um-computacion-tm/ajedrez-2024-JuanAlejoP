b315c0294fef23cf7af22e715266d894ff54109e
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-25 21:37:10 -0300

    add tests for bishop

--- tests/pieces/tests_bishop.py
+++ tests/pieces/tests_bishop.py
@@ -7,6 +7,14 @@ class TestBishop(unittest.TestCase):
         self.white_bishop = Bishop('WHITE')
         self.black_bishop = Bishop('BLACK')
 
+    def test_diagonal_move(self):
+        self.assertTrue(self.white_bishop.move(0, 2, 3, 5))
+        self.assertTrue(self.black_bishop.move(7, 2, 4, 5))
+
+    def test_invalid_move(self):
+        self.assertFalse(self.white_bishop.move(0, 2, 0, 5))
+        self.assertFalse(self.black_bishop.move(7, 2, 7, 0))
+
 
 
 if __name__ == '__main__':
