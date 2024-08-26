42fba1beb4e4ac1ea39b7e3418d2bbd0680e7566
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-25 21:58:41 -0300

    add tests for chess

--- tests/tests_chess.py
+++ tests/tests_chess.py
@@ -7,6 +7,49 @@ class TestChess(unittest.TestCase):
     def setUp(self):
         self.chess = Chess()
 
+    def test_initial_turn(self):
+        self.assertEqual(self.chess.turn, 'WHITE')
+
+    def test_move(self):
+        self.chess.move(1, 0, 3, 0)
+        self.assertIsInstance(self.chess.board.get_piece(3, 0), Pawn)
+        self.assertIsNone(self.chess.board.get_piece(1, 0))
+        self.assertEqual(self.chess.turn, 'BLACK')
+
+    def test_invalid_move_empty_square(self):
+        with self.assertRaises(ValueError):
+            self.chess.move(3, 3, 4, 3)
+
+    def test_invalid_move_blocked_path(self):
+        with self.assertRaises(ValueError):
+            self.chess.move(0, 0, 0, 3)
+
+    def test_capture(self):
+        self.chess.move(1, 0, 3, 0)
+        self.chess.move(6, 1, 4, 1)
+        self.chess.move(3, 0, 4, 1)
+        self.assertIsInstance(self.chess.board.get_piece(4, 1), Pawn)
+        self.assertEqual(self.chess.board.get_piece(4, 1).colour, 'WHITE')
+
+    def test_invalid_capture_same_color(self):
+        with self.assertRaises(ValueError):
+            self.chess.move(0, 0, 0, 1)
+
+    def test_pawn_double_step(self):
+        self.assertTrue(self.chess.is_valid_pawn_move(self.chess.board.get_piece(1, 0), 1, 0, 3, 0))
+        self.assertFalse(self.chess.is_valid_pawn_move(self.chess.board.get_piece(1, 0), 1, 0, 4, 0))
+
+    def test_pawn_diagonal_capture(self):
+        self.chess.move(1, 0, 3, 0)
+        self.chess.move(6, 1, 4, 1)
+        self.assertTrue(self.chess.is_valid_pawn_move(self.chess.board.get_piece(3, 0), 3, 0, 4, 1))
+
+    def test_turn_change(self):
+        self.assertEqual(self.chess.turn, 'WHITE')
+        self.chess.change_turn()
+        self.assertEqual(self.chess.turn, 'BLACK')
+        self.chess.change_turn()
+        self.assertEqual(self.chess.turn, 'WHITE')
 
 
 
