afca8134a6989a1b175b5bab0ed281ef6c6bf3f5
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-25 21:41:54 -0300

    add tests for knight

--- tests/pieces/tests_knight.py
+++ tests/pieces/tests_knight.py
@@ -7,6 +7,14 @@ class TestKnight(unittest.TestCase):
         self.white_knight = Knight('WHITE')
         self.black_knight = Knight('BLACK')
 
+    def test_l_shape_move(self):
+        self.assertTrue(self.white_knight.move(1, 0, 2, 2))
+        self.assertTrue(self.black_knight.move(7, 1, 5, 2))
+
+    def test_invalid_move(self):
+        self.assertFalse(self.white_knight.move(0, 1, 0, 0))
+        self.assertFalse(self.black_knight.move(7, 1, 0, 0))
+
 
 
 if __name__ == '__main__':
