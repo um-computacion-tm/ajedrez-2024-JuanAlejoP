273c38e1278098dcf77276bd7e5e213328cdeb3c
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-25 21:37:41 -0300

    add tests for king

--- tests/pieces/tests_king.py
+++ tests/pieces/tests_king.py
@@ -7,6 +7,14 @@ class TestKing(unittest.TestCase):
         self.white_king = King('WHITE')
         self.black_king = King('BLACK')
 
+    def test_single_square_move(self):
+        self.assertTrue(self.white_king.move(4, 4, 4, 5))
+        self.assertTrue(self.black_king.move(4, 4, 5, 4))
+
+    def test_invalid_move(self):
+        self.assertFalse(self.white_king.move(4, 4, 6, 4))
+        self.assertFalse(self.black_king.move(4, 4, 4, 6))
+
 
 
 if __name__ == '__main__':
