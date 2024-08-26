ee7a5af9f6dcffcad012c79fee2a639a28cb4c67
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-08-25 21:36:31 -0300

    add tests for queen

--- tests/pieces/tests_queen.py
+++ tests/pieces/tests_queen.py
@@ -7,6 +7,18 @@ class TestQueen(unittest.TestCase):
         self.white_queen = Queen('WHITE')
         self.black_queen = Queen('BLACK')
 
+    def test_horizontal_vertical_move(self):
+        self.assertTrue(self.white_queen.move(0, 0, 0, 7))
+        self.assertTrue(self.black_queen.move(0, 0, 7, 0))
+
+    def test_diagonal_move(self):
+        self.assertTrue(self.white_queen.move(0, 0, 7, 7))
+        self.assertTrue(self.black_queen.move(7, 7, 0, 0))
+
+    def test_invalid_move(self):
+        self.assertFalse(self.white_queen.move(0, 0, 2, 1))
+        self.assertFalse(self.black_queen.move(7, 7, 5, 6))
+
 
 
 if __name__ == '__main__':
