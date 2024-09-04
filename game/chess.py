1c1e07b8bcf0c89bac9695c76d1dc480e54b993d
JuanAlejoP <j.patino@alumno.um.edu.ar>
2024-09-04 18:55:16 -0300

    refactor chess code

--- game/chess.py
+++ game/chess.py
@@ -24,19 +24,22 @@ class Chess:
         if piece is None:
             raise ValueError('No hay pieza en esa posición.')
         if not self.is_valid_move(piece, from_row, from_col, to_row, to_col):
-            raise ValueError('Movimiento inválido para esta pieza.')
-        if not isinstance(piece, Knight) and self.__board__.is_path_blocked(from_row, from_col, to_row, to_col):
+            raise ValueError('Movimiento inválido para esa pieza.')
+        if not isinstance(piece, Knight) and \
+            self.__board__.is_path_blocked(from_row, from_col, to_row, to_col):
             raise ValueError('El camino está bloqueado por otra pieza.')
         if piece.colour != self.__turn__:
             raise ValueError(f'Es turno de las {self.turn}.')
         if target_piece and target_piece.colour == piece.colour:
-            raise ValueError('No puedes capturar una pieza del mismo color.')
-        
+            raise ValueError('No podés capturar piezas de tu mismo color.')
         if not self.__board__.has_pieces(self.__turn__):
-            self.end_game(f"¡El jugador {self.__turn__} se quedó sin piezas! El otro jugador gana.")
+            self.end_game(f'¡No quedan piezas {self.__turn__}!')
+            self.change_turn()
+            print(f'¡Ganan las {self.__turn__}!')
         self.__board__.move_piece(from_row, from_col, to_row, to_col)
         if isinstance(target_piece, King):
-            self.end_game(f'¡El rey {target_piece.colour} ha sido capturado! {self.__turn__} gana.')
+            self.end_game(f'¡El rey {target_piece.colour} ha sido capturado!')
+            print(f'¡Ganan las {self.__turn__}!')
         else:
             self.change_turn()
 
@@ -53,10 +56,12 @@ class Chess:
         if from_col == to_col:
             if to_row == from_row + direction and not self.__board__.is_occupied(to_row, to_col):
                 return True
-            if (from_row == 1 or from_row == 6) and to_row == from_row + 2 * direction and not self.__board__.is_occupied(to_row, to_col):
+            if (from_row == 1 or from_row == 6) and to_row == from_row + 2 * direction and not \
+                self.__board__.is_occupied(to_row, to_col):
                 return True
         elif abs(from_col - to_col) == 1 and to_row == from_row + direction:
-            if self.__board__.is_occupied(to_row, to_col) and self.__board__.get_piece(to_row, to_col).colour != pawn.colour:
+            if self.__board__.is_occupied(to_row, to_col) and \
+                self.__board__.get_piece(to_row, to_col).colour != pawn.colour:
                 return True
         return False
 
